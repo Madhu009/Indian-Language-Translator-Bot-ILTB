e175d9dc4b5213b9e5ed09637765f55ba94e937e
Madhu <s.madhu.akp@gmail.com>
2017-07-05 15:21:43 +0530

    modified code for FB verification

--- facebook-bot-server/service.py
+++ facebook-bot-server/service.py
@@ -4,24 +4,36 @@ import json
 
 app=Flask(__name__)
 
-PAT=""
+PAT="EAAYX07LLHJwBAEFmDTAZBVPh7XSkJ4mB5xGfIUfX73DIYJ0FLLyfFR3opTWZAnEOEt4wBlRebt6yLgzcIrd635CrUTwXW4POXoa7GmG0pHjwoQZAuD3pRpZBNzL5c5orZAolZBfrO7fEYQ1RaMYrktZAEyZB50yAiKD9jYE4uT03nwZDZD"
 
-@app.route("/b",methods=['POST'])
+
+@app.route("/",methods=['POST'])
 def get_response():
     req = request.get_data()
     print(req)
 
     for sender, message in messaging_events(req):
         print("Incoming from {}: {}".format(sender, message))
-        #send_message(PAT, sender, message)
+        send_message(PAT, sender, message)
     return "ok"
 
+
+@app.route('/', methods=['GET'])
+def handle_verification():
+  print ("Handling Verification.")
+  if request.args.get('hub.verify_token', '') == 'token_verified':
+    print ("Verification successful!")
+    return request.args.get('hub.challenge', '')
+  else:
+    print ("Verification failed!")
+    return 'Error, wrong validation token'
 
 def messaging_events(req):
   data = json.loads(req)
   messaging_events = data["entry"][0]["messaging"]
   for event in messaging_events:
-    if "message" in event and "text" in event["message"]:
+    if "message" in event and "text" in event["message"] \
+            and "attachments" not in event["message"] and "sticker_id" not in event["message"]:
       yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
     else:
       yield event["sender"]["id"], "I can't echo this"
